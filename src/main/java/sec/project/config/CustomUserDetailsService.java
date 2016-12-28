82b7ba5dd1d62319db13853c43d0d3b7e76be01b
marrbjorn <dreamr@protonmail.ch>
2016-12-29 01:13:37 +0400

    Add files via upload

---
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -0,0 +1,60 @@
+package sec.project.config;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.annotation.PostConstruct;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private Map<String, String> accountDetails;
+
+    @PostConstruct
+    public void init() {
+        this.accountDetails = new TreeMap<>();
+        this.accountDetails.put("Valravn","(+F?%2^hXV=)2PbE@&;dJ4D]{SkZ_`Sf94n%S]uaNVP>;2w<sB?2^g");
+        this.accountDetails.put("Wardruna","fh2T[_6H h9ZfHJWn%pFxA_UP\"A^ym2`N+{3ge_'2&\\V~dFVAP*}Q/M");
+        this.accountDetails.put("Ilmarinen",";.;vmRh>Xd'5<rZZ[5uBHgcHs3G`c7WBRe3*%Tw]y&'cDL-Qe<HkV:Z9#");
+        this.accountDetails.put("Karhu","d'Y].=;msd:)fg)a<dzN{_~[`V5bj.S_fAe4&)U*/~m>Y,37>}3K'jb47^\\dds*m");
+        this.accountDetails.put("Asgeir","Fylkir");
+        this.accountDetails.put("Eivor","Sinklars Visa");
+        this.accountDetails.put("Heilung","Krigsgaldr");
+        this.accountDetails.put("Skogvidr","D%GfS_s5{@Vb2{,U9LBc!wv7XG4U5B&8\"r=u?Y;:;&4+34(>S\"Y'VkS89Q");
+        this.accountDetails.put("admin","admin");
+        this.accountDetails.put("CMS","CMSpassword");
+    //    ^ we able to remove this "default" passwords for "admin"-access;
+   //       or passwords, which can be as "backdoor" under the "tools in use";
+  //        but also will use "encrypt"-methods for store passwords in database;      
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!this.accountDetails.containsKey(username)) {
+            throw new UsernameNotFoundException("No such user: " + username);
+        }
+        if (this.accountDetails.get(username).equals("admin")){
+        return new org.springframework.security.core.userdetails.User(
+             username,
+                this.accountDetails.get(username),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("ADMIN")));
+        }
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                this.accountDetails.get(username),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("USER")));
+    }
+}
---
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package sec.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+//  we have to use this import - if want to use encrypt-method for passwords:
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+  // we able to remove ^ this string for turn ON "default" feature back
+ // under the Spring Framework for CSRF protection enabled;
+  
+        http.authorizeRequests()
+                .antMatchers("/hidden", "/css/**").permitAll()
+      // we able to remove ^ this string for remove "debug"-page-(access)
+     // when application goes be visible for users;
+                .antMatchers("/formsrc").hasAuthority("ADMIN")
+                .antMatchers("/fylkr").authenticated()
+                .anyRequest().authenticated();
+        http.formLogin().permitAll();
+    //  ^ good to think about logout too;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
+/* 
+  // We able to add this method for provide option: encrypt passwords;
+ // Additionally to this method we have re-design also previous method:
+// auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+   
+    @Bean
+    public PasswordEncoder passwordEncoder()
+                                    {return new BCryptPasswordEncoder();}
+*/
